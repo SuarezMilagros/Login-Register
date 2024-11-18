cb2d7570f928f3ad41e7356f2ed4c9455fbb085d
SuarezMilagros <milagros.16.princesa@gmail.com>
2024-11-17 21:34:57 -0300

    Primer parte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
